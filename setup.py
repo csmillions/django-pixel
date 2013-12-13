c6cd72026a3f1ce58682921ccd705a9d5a42071b
Chris Modjeska <kurisui@gmail.com>
2013-12-13 10:24:51 -0500

    Initial commit to new repo

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import django_pixel
+
+setup(
+	name = "cmsplugin-pixel",
+	version = cmsplugin_pixel.__version__,
+	packages = find_packages(),
+
+	author = "Chris Modjeska",
+	author_email = "kin@remuria.net",
+	description = "",
+	keywords = "django",
+	url = "https://github.com/csmillions/cmsplugin-javascript/",
+	include_package_data=True,
+	long_description= """
+	""",
+	install_requires=[
+		"django",
+		"django-cms"
+	],
+)
